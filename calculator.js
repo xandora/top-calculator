0896ffbd91654535117f3d4ebd0156d2e81a5489
Michael Frank <mfra109@gmail.com>
2021-09-30 14:41:47 +1300

    Fleshed out most functionality roughly.

--- calculator.js
+++ calculator.js
@@ -1,29 +1,80 @@
+let register = "";
+let inputA = "";
+let inputB = "";
+let operator = "";
+
 const add = function (a, b) {
     return a + b;
-}
+};
 const subtract = function (a, b) {
     return a - b;
-}
+};
 const multiply = function (a, b) {
     return a * b;
-}
+};
 const divide = function (a, b) {
     return a / b;
-}
+};
 
 const operate = function (a, b, operator) {
     switch(operator) {
         case '+':
-            return add(a, b);
+            screen.textContent = add(a, b);
             break;
         case '-':
-            return subtract(a, b);
+            screen.textContent = subtract(a, b);
             break;
         case '*':
-            return multiply(a, b);
+            screen.textContent = multiply(a, b);
             break;
         case '/':
-            return divide(a, b);
+            screen.textContent = divide(a, b);
             break;
     }
-}
+};
+
+const updateScreen = function(button) {
+    if(button.classList.contains("number")) {
+        if(screen.textContent == "0"){
+            screen.textContent = button.textContent;
+        }
+        else if(inputB != "") {
+            screen.textContent = button.textContent;
+            inputA = "";
+            inputB = "";
+            operator = "";
+        }
+        else {
+            screen.textContent += button.textContent;
+        }
+    }
+    else if(button.classList.contains("dot")) {
+        if(screen.textContent.includes(".")){
+
+        }
+        else {
+            screen.textContent += "."
+        }
+    }
+    else if(button.classList.contains("operator")) {
+        register = screen.textContent;
+        operator = button.textContent;
+        screen.textContent = "";
+    }
+    else if(button.classList.contains("equals")) {
+        inputA = register;
+        inputB = screen.textContent;
+        operate(parseFloat(inputA), parseFloat(inputB), operator);
+    }
+};
+
+const screen = document.body.querySelector("#display");
+const buttons = document.body.querySelectorAll(".button");
+
+screen.textContent = 0;
+
+buttons.forEach((button) => {  
+    button.addEventListener('click', () => {
+        updateScreen(button);
+    })
+});
